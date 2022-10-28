a96dcb3f56b6f85de6b32dd7cea1919b3ca51e44
Christopher <chrito83340@hotmail.com>
2022-10-28 11:30:46 +0200

    finalisation code

--- ClashProcess/Main/Src/Random.c
+++ ClashProcess/Main/Src/Random.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
-int random(int nbfic);
+int alea(int nbfic);
+void verif(int nbmax);
 
 int main(void)
 {
     srand(time(NULL));
     int max;
-    printf("\nEntrer le nombre max du random\n");
+    printf("\nEntrer le nombre max du alea\n");
     scanf("%d",&max);
-    printf("\nLe nom de votre fichier est : %d \n",random(max));
+    printf("\nLe nom de votre fichier est : %d \n",alea(max));
 
+    verif(max);
+
+    return 0;
 }
 
-int random(int nbfic)
+int alea(int nbfic)
 {
-    return rand(nbfic);
+    int aleanum;
+    aleanum = rand() % nbfic + 1;
+    return aleanum;
 }
 
 
@@ -25,8 +32,9 @@ void verif(int nbmax)
     int randomnb;
     while(randomnb <= nbmax)
     {
-        randomnb = random(nbmax);
-        printf("\nLe nom de votre fichier est : %d \n");
+        randomnb = alea(nbmax);
+        printf("\nLe nom de votre fichier est : %d \n",randomnb);
+        sleep(1);
     }
     printf("\npas bon !!!!\n");
 }
